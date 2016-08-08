d6546847111bb92eb2c49c4843ca5fa4cb90a55f
Damien Robinson <damien.robinson@griffithuni.edu.au>
2016-08-08 11:31:11 +0000

    changed the search submit to a search button

---
+++ lab3/js/search.js
@@ -0,0 +1,8 @@
+var figures = document.getElementsByTagName("figure");
+var figcaption = figures[0].innerHTML;
+document.getElementById("search-btn").onclick = displayThumb;
+console.log(figcaption);
+
+function displayThumb() {
+    console.log("dicks");
+}
