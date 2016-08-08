1c84b648a82b15e99e312e28ae598a3d6fa8e5cb
Damien Robinson <damien.robinson@griffithuni.edu.au>
2016-08-08 12:03:32 +0000

    added changing every second thumbnail background black

--- lab3/js/search.js
+++ lab3/js/search.js
@@ -5,13 +5,18 @@ console.log(figcaption);
 
 /**
  * Read the searchFeild input, then displays the thumbnail the number of times 
- * based on the number ented into the searchField.
+ * based on the number ented into the searchField. Changes the background of 
+ * every second thumbnail to be black.
  */
 function displayThumb() {
     var num = document.getElementById("searchField").value;
     var htmlStr = "";
-    for (var i = 0; i < num; i++) {
+    for (var i = 0; i < num; i++){
+        if (i % 2 === 0){
+             htmlStr += '<figure style="background: black;">' + figcaption + '</figure>';
+        }else {
         htmlStr += '<figure>' + figcaption + '</figure>';
+        }
     }
     var thumbs = document.getElementById("thumbnails");
     thumbs.innerHTML = htmlStr;
