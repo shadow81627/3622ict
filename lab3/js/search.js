8e19f691b2461e9473f767bbc9cc69de0f889a0d
Damien Robinson <damien.robinson@griffithuni.edu.au>
2016-08-08 11:53:49 +0000

    added displaying the first thumbnail based on the number enter into the search field

--- lab3/js/search.js
+++ lab3/js/search.js
@@ -3,6 +3,17 @@ var figcaption = figures[0].innerHTML;
 document.getElementById("search-btn").onclick = displayThumb;
 console.log(figcaption);
 
+/**
+ * Read the searchFeild input, then displays the thumbnail the number of times 
+ * based on the number ented into the searchField.
+ */
 function displayThumb() {
-    console.log("dicks");
+    var num = document.getElementById("searchField").value;
+    var htmlStr = "";
+    for (var i = 0; i < num; i++) {
+        htmlStr += '<figure>' + figcaption + '</figure>';
+    }
+    var thumbs = document.getElementById("thumbnails");
+    thumbs.innerHTML = htmlStr;
+    console.log(htmlStr);
 }
