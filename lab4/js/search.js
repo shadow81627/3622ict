6725eaaa8ac8ccdacac659149df05eb00965c5ae
Damien Robinson <damien.robinson@griffithuni.edu.au>
2016-08-13 10:36:49 +0000

    added search submit function

--- lab4/js/search.js
+++ lab4/js/search.js
@@ -1,10 +1,10 @@
 //photo data
 var photoData = [
-    {url: "photos/DSC01049.JPG", description:"City View"},
-    {url: "photos/DSC01066.JPG", description:"Ferris Wheel"},
-    {url: "photos/DSC02511.jpg", description:"A Building in the forbidden city with extra long text"},
-    {url: "photos/DSC03810.jpg", description:"City from Mt Gravett look out"},
-    {url: "photos/DSC05750.jpg", description:"Sun rise"}
+    {url: "photos/DSC01049.JPG", description: "City View"},
+    {url: "photos/DSC01066.JPG", description: "Ferris Wheel"},
+    {url: "photos/DSC02511.jpg", description: "A Building in the forbidden city with extra long text"},
+    {url: "photos/DSC03810.jpg", description: "City from Mt Gravett look out"},
+    {url: "photos/DSC05750.jpg", description: "Sun rise"}
 ];
 //gets all the figures
 var figures = document.getElementsByTagName("figure");
@@ -15,7 +15,10 @@ var thumbs = document.getElementById("thumbnails");
 //gets the original thumb nail content
 var originalThumb = thumbs.innerHTML;
 //gets the search button
-document.getElementById("search-btn").onclick = search;
+document.getElementById("search").addEventListener("submit", function(event){
+    event.preventDefault();
+});
+document.getElementById("search").addEventListener("submit", search);
 //gets the login button
 document.getElementById("login-btn").onclick = loginAlert;
 
@@ -26,28 +29,31 @@ document.getElementById("login-btn").onclick = loginAlert;
  */
 function search() {
     //get seachFeild text
-    var query = document.getElementById("searchField").value.trim();
+    var query = document.getElementById("searchField").value.toLowerCase().trim();
     //get error html location
     var error = document.getElementById("error"); 
+    error.innerHTML = "";
     //results array
-    var results = [];
+    var result = [];
 
     //if the query is not empty
     if (query.length > 0){
-        //set the error to be empty
-        error.innerHTML = "";
         //iterate through the photoData
         for (var i = 0; i < photoData.length; i++){
             //compare photo description with query, if it matches then add photo to results
-            if (photoData[i].description.indexOf(query) != -1) {
+            if (photoData[i].description.toLowerCase().indexOf(query) != -1) {
                  result.push(photoData[i]);
             }else {
                 //dont do anything
             }
         }
-        displayThumb(result);
+        if (result.length > 0) {
+            displayThumb(result);
+        }else {
+            error.innerHTML = "No matching photo found";
+        }
+        
     }else {
-        error.innerHTML = 'Please enter a valid query';
         thumbs.innerHTML = originalThumb;
     }
 }
@@ -58,7 +64,7 @@ function search() {
 function displayThumb(result) {
     var htmlStr = "";
     for (var i = 0; i < result.length; i++){
-         htmlStr += '<figure>' + figcaption + '</figure>';
+         htmlStr +=  '<figure><a href="' + result[i].url + '"><img src="' + result[i].url + '" alt="' + result[i].description + '" height="200" width="200"></a><figcaption>' + result[i].description + '</figcaption></figure>'
     }
     thumbs.innerHTML = htmlStr;
 }
