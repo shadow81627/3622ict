659564c25bd88c461d40178de3765e96698e62df
Damien Robinson <damien.robinson@griffithuni.edu.au>
2016-10-20 00:05:01 +0000

    Added header to index page

--- Assignment/app.js
+++ Assignment/app.js
@@ -2,10 +2,13 @@ var app = angular.module("app", ["ngRoute"]);
 
     app.config(function($routeProvider) {
       $routeProvider
-      .when("/", {
-        templateUrl : "Views/splash.html"
-    })
-      .when("/splash", {
+        .when("/", {
+            templateUrl : "Views/splash.html"
+        })
+        .when("/splash", {
           templateUrl : "Views/content.html"
-      });
+        })
+        .otherwise({
+            templateUrl : "Views/splash.html"
+        });
     });
