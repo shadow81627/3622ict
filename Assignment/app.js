64a4303e09c96c7f7c095b23107b587be7aae8d3
Damien Robinson <damien.robinson@griffithuni.edu.au>
2016-10-21 06:06:23 +0000

    Added facebook login

--- Assignment/app.js
+++ Assignment/app.js
@@ -1,7 +1,6 @@
 var app = angular.module("app", ["ngRoute"]);
 var controllers = {};
-app.controller(controllers);
-  
+
 app.config(function($routeProvider) {
   $routeProvider
     .when("/", {
@@ -15,3 +14,10 @@ app.config(function($routeProvider) {
     });
 });
 
+controllers.loginController = function($scope) {
+  var APP_ID = "342478882810704";
+  var APP_URL = "https://s5014219-s5014219.c9users.io/3622ICT/Assignment/";
+  $scope.loginURL = "https://www.facebook.com/dialog/oauth?client_id=" + APP_ID + "&redirect_uri=" + APP_URL + "&response_type=token";
+};
+
+app.controller(controllers);
--- Assignment/facebook-login.js
+++
@@ -1,8 +0,0 @@
-var APP_ID = "342478882810704";
-var APP_URL = "https://s5014219-s5014219.c9users.io/3622ICT/Assignment/";
-var loginURL = "https://www.facebook.com/dialog/oauth?client_id=" + APP_ID + "&redirect_uri=" + APP_URL + "&response_type=token";
-
-controllers.loginController = function($scope) {
-  $scope.APP_ID = "342478882810704";
-  $scope.APP_URL = "https://s5014219-s5014219.c9users.io/3622ICT/Assignment/";
-};
