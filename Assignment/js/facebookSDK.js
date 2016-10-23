42c6ca9abebefa020b6026c123e5124080b51f74
Damien Robinson <damien.robinson@griffithuni.edu.au>
2016-10-23 13:35:09 +0000

    Added jquery setup of facebook SDK

--- Assignment/js/facebookSDK.js
+++ Assignment/js/facebookSDK.js
@@ -90,3 +90,19 @@ $(document).ready(function() {
   });
 });
 */
+
+$(function(){
+    $.ajaxSetup({ cache: true });
+    $.getScript('//connect.facebook.net/en_US/sdk.js', 
+    function(){
+        // This function is called after sdk.js has been loaded
+        FB.init({ 
+            appId: ‘123456789', 
+            version:’v2.7’
+        });
+        //  Any code that uses SDK goes here or gets called from here. E.g.:
+        $(‘#login’).click(function(){
+        model.login();  // call a custom function to log in to Facebook
+        });
+    });
+});
