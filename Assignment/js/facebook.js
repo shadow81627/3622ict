2e52f37dfc36083258ead358247760ab4d4afac1
Damien Robinson <damien.robinson@griffithuni.edu.au>
2016-10-23 10:42:05 +0000

    Removed test code from facebook js

--- Assignment/js/facebook.js
+++ Assignment/js/facebook.js
@@ -12,8 +12,7 @@
     if (facebook.access_token == "") {
       // attempts to get access token from the current URL.
       var url = window.location.href;
-      console.log("original url " + url)
-      
+
       //splits base url from parameter
       var urlBits = url.split('?');
       //splits parameter
@@ -23,26 +22,15 @@
       //iterate over the string and remove the equal character
       for (var i = 0; i < params.length; i++){
           paramsValue = params[i].split('=');
-          console.log(paramsValue);
           if (paramsValue[0].includes('access_token')){
-            facebook.access_token = paramsValue[1];
           }
           
       }
-      console.log(facebook.access_token);
-      /*var splitURL = url.split("?");
-      if (splitURL.length == 2) {
-        splitURL = splitURL[1].split("=");
-        //splitURL = splitURL[1].split("&");
-        // if token is in the URL, assign it to model.access_token...
-        facebook.access_token = splitURL[1];
-        console.log(facebook.access_token);
-      }*/
     }
     if (facebook.access_token != "") {
       // access token is available
       return true;
     } else {  // start user authentication
-      //window.location.replace(facebook.loginURL);
+      window.location.replace(facebook.loginURL);
     }
   };
